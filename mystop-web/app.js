661f3c9ebc3124eab1fc83ae1eed0b42b27835d1
alexanderbuck <alex.buck10@gmail.com>
2015-02-07 15:01:47 +0000

    more stuff

--- mystop-web/app.js
+++ mystop-web/app.js
@@ -1,11 +1,35 @@
-(function(){
-    var app = angular.module('mystop',[]);
+(function () {
+    var app = angular.module('mystop', ['ngRoute']);
 
-    app.controller('BusesController',['$scope',function($scope){
+    app.controller('BusesController', ['$scope', function ($scope) {
 
-        $scope.buses=[{code:"2a", destination:"Bournemouth", currentLocation:"Lansdowne"},
-            {code:"1a", destination:"Christchurch", currentLocation:"Poole"},
-            {code:"1d", destination:"Castlepoint", currentLocation:"Salisbury"}];
+        $scope.buses = [{code: "2a", destination: "Bournemouth", currentLocation: "Lansdowne"},
+            {code: "1a", destination: "Christchurch", currentLocation: "Poole"},
+            {code: "1d", destination: "Castlepoint", currentLocation: "Salisbury"}];
     }]);
 
+    app.controller('DestinationController', ['$scope', function ($scope) {
+
+        $scope.stops = [{stopName:"Fishermans road "},{ stopName:"Derby Road"},{stopName: "Denver Road"}];
+    }]);
+
+
+    app.controller('DuringJourneyController', ['$scope', function ($scope) {
+
+    }]);
+
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'choose-bus.html',
+            controller: 'BusesController'
+        });
+        $routeProvider.when('/destination', {
+            templateUrl: 'destination.html',
+            controller: 'DestinationController'
+        });
+        $routeProvider.when('/duringjourney', {
+            templateUrl: 'during-journey.html',
+            controller: 'DuringJourneyController'
+        });
+    }])
 })();
